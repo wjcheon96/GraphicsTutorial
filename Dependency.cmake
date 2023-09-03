abaaf76e53b19807376b57e6b5909115043e7be4
천웅재 <woongjae@cheon-ungjaes-MacBook-Air.local>
2023-09-03 20:55:21 +0900

    feat: 화면 생성

--- Dependency.cmake
+++ Dependency.cmake
@@ -8,7 +8,7 @@ set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
 # spdlog: fast logger library
 # spdlog를 다운받아서 가져옴. 로그를 보는데 용이.
 ExternalProject_Add(
-    dep-spdlog
+    dep_spdlog
     GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
     GIT_TAG "v1.x"
     GIT_SHALLOW 1
@@ -19,12 +19,12 @@ ExternalProject_Add(
 )
 
 # Dependency 리스트 및 라이브러리 파일 리스트 추가
-set(DEP_LIST ${DEP_LIST} dep-spdlog)
+set(DEP_LIST ${DEP_LIST} dep_spdlog)
 # OS 별 생성 파일이 .lib 와 .a 파일로 달라지므로, Mac 기준에 맞춰서 해당 디렉토리에 있는 링크를 제대로 맞춰줌.
 set(DEP_LIBS ${DEP_LIBS} libspdlog.a)
 # set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:Debug>:d>)
 
-# glfw
+# glfw 설치
 ExternalProject_Add(
     dep_glfw
     GIT_REPOSITORY "https://github.com/glfw/glfw.git"
@@ -41,4 +41,20 @@ ExternalProject_Add(
 )
 
 set(DEP_LIST ${DEP_LIST} dep_glfw)
-set(DEP_LIBS ${DEP_LIBS} libglfw3.a)
+set(DEP_LIBS ${DEP_LIBS} glfw3)
+
+# glad
+ExternalProject_Add(
+    dep_glad
+    GIT_REPOSITORY "https://github.com/Dav1dde/glad"
+    GIT_TAG "v0.1.34"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+        -DGLAD_INSTALL=ON
+    TEST_COMMAND ""
+    )
+set(DEP_LIST ${DEP_LIST} dep_glad)
+set(DEP_LIBS ${DEP_LIBS} glad)
--- src/main.cpp
+++ src/main.cpp
@@ -1,99 +1,99 @@
-#include <spdlog/spdlog.h>
-#include <GLFW/glfw3.h>
+#include "common.h"
+#include <iostream>
 
-int main() {
+// #include <spdlog/spdlog.h>
+// // glad는 반드시 glfw 이전에 헤더 명시를 해주어야한다.
+// #include <glad/glad.h>
+// #include <GLFW/glfw3.h>
 
-    // glfwInit();
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    SPDLOG_INFO("initialize glfw");
+// window의 크기가 바꼈을때 해야할 작업 수행.
+void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
+    SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
+    // glViewport를 통해 opengl이 그릴 영역을 지정해줌.
+    glViewport(0, 0, width, height);
+}
+
+void OnKeyEvent(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    SPDLOG_INFO("key:{}, scancode: {}, action: {}, mods: {}{}{}",
+        key, scancode,
+        action == GLFW_PRESS ? "Pressed":
+        action == GLFW_RELEASE ? "Released" :
+        action == GLFW_REPEAT ? " Repeat" : "Unknown",
+        mods & GLFW_MOD_CONTROL ? "C" : "-",
+        mods & GLFW_MOD_SHIFT ? "S" : "-",
+        mods&GLFW_MOD_ALT ? "A" : "-");
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main(int ac, char **av) {
+    std::cout << "Initialize glfw" << std::endl;
     if (!glfwInit()) {
         const char* description = NULL;
         glfwGetError(&description);
-        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        std::cout << "Failed to initialize glfw: " << description << std::endl;
         return -1;
     }
 
-    SPDLOG_INFO("Create glfw window");
-    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, NULL, NULL);
+    // init 이후 window 생성 전에 만들기 희망하는 OpenGL의 버전을 추가해줌.
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+    std::cout << "Create glfw window" << std::endl;
+    // glfwCreateWindow를 호출 시 window가 생성되며, 동시에 window에서 사용되는 openGL context가 만들어진다.
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
     if (!window) {
-        SPDLOG_ERROR("failed to create glfw window");
+        std::cout << "failed to create glfw window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    SPDLOG_INFO("Start main loop");
+    // 따라서 glfwCreateWindow에서 생성된 openGL context를 사용하게끔 명시적으로 넣어줘야 한다.
+    glfwMakeContextCurrent(window);
+
+    // glfw로 생성된 윈도우에 특정 이벤트 발생시 실행되는 콜백함수 지정.
+    // 아래와 같이 glfwXXXCallback의 형태를 가짐.
+    // window와 콜백 함수를 매개변수로 집어넣는다.
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
+    glfwSetKeyCallback(window, OnKeyEvent);
+
+    // context 생성 이후에 glad 라이브러리를 통해 openGL함수를 로딩한다.
+    // process address를 얻어오는 함수를 통해, gl 함수를 로딩함.
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "failed to initialize glad" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // 정상적으로 gl함수가 잘 불려왔는지를 확인한다.
+    // opengl function 중에서 가장 기본적인 정보를 가져오는 glGetString을 통해 확인해본다.
+    auto glVersion = glGetString(GL_VERSION);
+    std::cout << "OpenGl context version: " << glVersion << std::endl;
+
     while (!glfwWindowShouldClose(window)) {
+        // 아래 루프문에서 event가 발생시 해당 event를 수집함.
         glfwPollEvents();
+        // window를 clear할때 clear할 색상 지정.
+        glClearColor(0.0f, 0.1f, 0.2f, 0.0f);
+        // 실제 framebuffer를 clear함.
+        glClear(GL_COLOR_BUFFER_BIT);
+        /*
+            Framebuffer swap은 화면에 그림을 그리는 과정으로,
+            프레임 버퍼를 2개를 준비하여(front / back)
+            front buffer에 바로 그리게 되면 그림이 그려지는 과정까지 보여 더러워지기에, 
+            back buffer에 그림을 그리고, front와 back을 바꿔치기 하면서 위 과정을 반복하여
+            그림이 그려지는 과정이 노출되지 않게 함.
+            double buffering이라고 함.
+        */
+        glfwSwapBuffers(window);
+
     }
 
     glfwTerminate();
-
     return 0;
 }
-
-
-// #include <glad/glad.h>
-// #include <GLFW/glfw3.h>
-// #include <iostream>
-
-// int main() {
-
-//     std::cout << "hello world" << std::endl;
-
-//     // Initialize GLFW
-//     glfwInit();
-
-//     // Tell GLFW what version of OpenGL we are using
-//     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);`
-//     // Tell GLFW we are using the CORE profile
-//     // So that means we only have the modern function
-//     // Tell it that we don't use the useless function of the lower version.
-//     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-//     // When making GLFW window, without this window hint, doesn't run when using Mac OS
-//     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
-
-//     // Create a GLFW window object. width, height, name
-//     GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL", NULL, NULL);
-//     if (window == NULL) {
-//         std::cout << "Failed to create GLFW window" << std::endl;
-//         glfwTerminate();
-//         return -1;
-//     }
-
-//     // Introduce the window into the current context
-//     glfwMakeContextCurrent(window);
-
-//     // Load GLAD so it configures OpenGL
-//     gladLoadGL();
-
-//     // Set the ViewPoint. Tell OpenGL the size of the window.
-//     glViewport(0, 0, 800, 800);
-
-//     // Specify the color of the background
-//     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-    
-//     // Clean the back buffer and assign the new color to it. GL_COLOR_BUFFER_BIT means the buffer which is used for write the color
-//     glClear(GL_COLOR_BUFFER_BIT);
-
-//     // Swap the back buffer with the front buffer.
-//     glfwSwapBuffers(window);
-
-//     // Main loop
-//     while (!glfwWindowShouldClose(window)) {
-//         // take car of all GLFW Events
-//         glfwPollEvents();
-//     }
-
-//     // Delete the window before ending the program.
-//     glfwDestroyWindow(window);
-//     // Terminate GLFW before ending the program.
-//     glfwTerminate();
-
-//     return 0;
-// }
