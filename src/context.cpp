c35aa61e334cdaa3642ea4a79bef15ffa8bedba9
천웅재 <woongjae@cheon-ungjaes-MacBook-Air.local>
2023-09-05 11:53:07 +0900

    feat: vertex attribute에 정보 여러가지 집어넣기

--- src/context.cpp
+++ src/context.cpp
@@ -13,10 +13,10 @@ bool Context::Init() {
     // 정점 데이터를 담은 array를 선언.
     // openGL화면은 가로 세로가 -1 ~ +1로 normalizing 된 좌표계를 사용한다.
     float vertices[] = {
-        0.5f, 0.5f, 0.0f, // top right
-        0.5f, -0.5f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f, // top left
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right, red
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right, green
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left, blue
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, // top left, yellow
     };
 
     uint32_t indices[] = { // note that we start from 0!
@@ -26,18 +26,21 @@ bool Context::Init() {
 
     m_vertexLayout = VertexLayout::Create();
     m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, 
-        GL_STATIC_DRAW, vertices, sizeof(float) * 12);
+        GL_STATIC_DRAW, vertices, sizeof(float) * 24);
 
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, 
-        sizeof(float) * 3, 0);
+    // 0번 attribute만 쓰는것이 아닌, 1번 attribute도 써서, 0번에 위치, 1번에 색상을 지정하게끔 하려 함.
+    // stride가 증가한 이유는, 하나의 점을 표시를 할 때 2개의 attribute를 담기에, 6개가 된다.
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3);
+    // m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, 
+    //     sizeof(float) * 3, 0);
 
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER,
         GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
 
-
     // gl function이 생성된 이후에 쉐이더를 호출해서 사용해야함.
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
     if (!vertShader || !fragShader)
         return false;
     SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
@@ -49,13 +52,6 @@ bool Context::Init() {
         return false;
     SPDLOG_INFO("program id: {}", m_program->Get());
 
-    // 프로그램 인스턴스의 아이디를 가져와서 세팅한다.
-    // color라는 string을 집어넣는데, 이는 uniform variable의 변수 명이 된다.
-    // return되는 에 glUniform4f()의 인자로 호출을 해서 가져온다.
-    auto loc = glGetUniformLocation(m_program->Get(), "color");
-    m_program->Use();
-    glUniform4f(loc, 1.0f, 1.0f, 0.0f, 1.0f);
-
     // window를 clear할때 clear할 색상 지정.
     glClearColor(0.0f, 0.1f, 0.2f, 0.0f);
     return true;
@@ -65,7 +61,19 @@ void Context::Render() {
     // 실제 framebuffer를 clear함.
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // // static으로 선언했기에, 매 프레임마다 반복적으로 실행될때마다 값이 증가하게 된다.
+    // static float time = 0.0f;
+    // float t = sinf(time) * 0.5f + 0.5f;
+    // // 프로그램 인스턴스의 아이디를 가져와서 세팅한다.
+    // // color라는 string을 집어넣는데, 이는 uniform variable의 변수 명이 된다.
+    // // return되는 에 glUniform4f()의 인자로 호출을 해서 가져온다.
+    // // 해당 program을 쓰면서, color 변수를 uniform variable로 지정한다.
+    // auto loc = glGetUniformLocation(m_program->Get(), "color");
     m_program->Use();
+    // // 색상 변화를 지정해서 넣어준다.
+    // glUniform4f(loc, t*t, 2.0f*t*(1.0f-t), (1.0f-t)*(1.0f-t), 1.0f);
     // glDrawArrays(GL_TRIANGLES, 0, 6);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    // time += 0.016f;
 }
