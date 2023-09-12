c6e1c672fb4b32f48579139dde6b52feff0ccbef
천웅재 <woongjae@cheon-ungjaes-MacBook-Air.local>
2023-09-12 10:21:30 +0900

    fix: 쓸모없는 주석 제거

--- src/main.cpp
+++ src/main.cpp
@@ -104,16 +104,10 @@ int main(int ac, char **av) {
         glfwTerminate();
         return -1;
     }
+    // resizing시, mac 의 경우 픽셀을 1단위픽셀을 2배로 잡기에 생기는 문제를 해결하기 위함.
     glfwSetWindowUserPointer(window, context.get());
     SPDLOG_INFO("scale factor: {}", scaleFactor);
     OnFramebufferSizeChange(window, WINDOW_WIDTH * scaleFactor, WINDOW_HEIGHT * scaleFactor);
-
-    // resizing시, mac 의 경우 픽셀을 1단위픽셀을 2배로 잡기에 생기는 문제를 해결하기 위함.
-    // #ifdef __APPLE__
-    //     OnFramebufferSizeChange(window, WINDOW_WIDTH * 2, WINDOW_HEIGHT * 2);
-    // #else
-    //     OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
-    // #endif
 
     // glfw로 생성된 윈도우에 특정 이벤트 발생시 실행되는 콜백함수 지정.
     // 아래와 같이 glfwXXXCallback의 형태를 가짐.
